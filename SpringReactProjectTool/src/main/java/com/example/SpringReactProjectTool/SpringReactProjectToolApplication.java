4454f66d68983c617e5b98ac21c4da5adbd8ddda
Rohit Sharma <rohitsharma050498@gmail.com>
2021-05-31 23:09:09 +0530

    first Commit

---
+++ SpringReactProjectTool/src/main/java/com/example/SpringReactProjectTool/SpringReactProjectToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringReactProjectTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactProjectToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactProjectToolApplication.class, args);
+	}
+
+}
