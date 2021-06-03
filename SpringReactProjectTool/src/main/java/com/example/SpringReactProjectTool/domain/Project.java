e90822268b06818197852ff040357f0bd4f3a2cb
Rohit Sharma <rohitsharma050498@gmail.com>
2021-06-03 10:28:40 +0530

    Prject.java updated

--- SpringReactProjectTool/src/main/java/com/example/SpringReactProjectTool/domain/Project.java
+++ SpringReactProjectTool/src/main/java/com/example/SpringReactProjectTool/domain/Project.java
@@ -2,7 +2,17 @@ package com.example.SpringReactProjectTool.domain;
 
 import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -11,14 +21,22 @@ public class Project {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	
 	private Long id;
+	@NotBlank(message="Project Name is required")
 	private String projectName;
+	@NotBlank(message="Project Identifier is required")
+	@Size(min=4,max=5,message="please use 4 or 5 characters")
+	@Column(updatable=false, unique=true)
 	private String projectIdentifier;
+	@NotBlank(message="Project Description  is required")
 	private String description;
+	@JsonFormat(pattern="dd-mm-yyyy")
 	private Date start_date;
+	@JsonFormat(pattern="dd-mm-yyyy")
 	private Date end_date;
 	
-	
+	@JsonFormat(pattern="dd-mm-yyyy")
 	private Date created_At;
+	@JsonFormat(pattern="dd-mm-yyyy")
 	private Date updated_At;
 	
 	
