009348c1d4faf66379f7b49e7aaf80807b9de8e7
Rohit Sharma <rohitsharma050498@gmail.com>
2021-06-05 00:45:46 +0530

    added important custome Exception

--- SpringReactProjectTool/src/main/java/com/example/SpringReactProjectTool/services/ProjectService.java
+++ SpringReactProjectTool/src/main/java/com/example/SpringReactProjectTool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.SpringReactProjectTool.domain.Project;
+import com.example.SpringReactProjectTool.exception.ProjectIdException;
 import com.example.SpringReactProjectTool.repository.ProjectRepository;
 
 @Service
@@ -15,7 +16,13 @@ public class ProjectService {
 	
 	 public Project saveOrUpdateProject(Project project) {
 
-	        return projectRepository.save(project);
+	       try {
+	    	   project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+	    	   return projectRepository.save(project);
+	       }catch(Exception e)
+	       {
+	    	   throw new ProjectIdException("Project ID "+project.getProjectIdentifier().toUpperCase()+" already exists");
+	       }
 	    }
 
 }
