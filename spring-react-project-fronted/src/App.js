2d7307fc95b6c0f3d5f5359e579528928552f73c
Rohit Sharma <rohitsharma050498@gmail.com>
2021-06-08 22:51:27 +0530

    first commit to add the react for fronted

---
+++ spring-react-project-fronted/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
