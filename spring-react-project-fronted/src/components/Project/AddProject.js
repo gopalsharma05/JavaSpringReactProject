e54d00177c3445af12da55864bf8dd07bdc59a23
Rohit Sharma <rohitsharma050498@gmail.com>
2021-06-09 17:02:30 +0530

    first functional component route

---
+++ spring-react-project-fronted/src/components/Project/AddProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is the project form</h1>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
