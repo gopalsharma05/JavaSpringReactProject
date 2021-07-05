ce7f80d37de6ecb5aa7722677172a636e17c1829
Rohit Sharma <rohitsharma050498@gmail.com>
2021-07-05 19:45:23 +0530

    updateProject complete

--- spring-react-project-fronted/src/actions/ProjectActions.js
+++ spring-react-project-fronted/src/actions/ProjectActions.js
@@ -5,6 +5,10 @@ export const CreateProject = (project, history) => async (dispatch) => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}, // No error should be there in state, if project created successfully
+    });
   } catch (err) {
     // console.log(err.response.data);  // to see the error else, if we do not use correct id then 400 error will come
     dispatch({
@@ -23,10 +27,14 @@ export const GetProjects = () => async (dispatch) => {
 };
 
 export const GetProject = (id, history) => async (dispatch) => {
-  // if we want to pass the parameter with the url, use the below syntax
-  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
-  dispatch({
-    type: GET_PROJECT,
-    payload: res.data,
-  });
+  try {
+    // if we want to pass the parameter with the url, use the below syntax
+    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    dispatch({
+      type: GET_PROJECT,
+      payload: res.data,
+    });
+  } catch (error) {
+    history.push("/dashboard");
+  }
 };
