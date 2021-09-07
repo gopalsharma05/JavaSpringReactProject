0878963a9a847ad4f5561bc41d9a5ab9432688e9
Rohit Sharma <rohit.sharma@comviva.com>
2021-09-07 12:08:42 +0530

    ProjectTask Reducers , action, form added

--- spring-react-project-fronted/src/components/Project/ProjectItem.js
+++ spring-react-project-fronted/src/components/Project/ProjectItem.js
@@ -25,11 +25,11 @@ class ProjectItem extends Component {
             </div>
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
-                <a href="#">
+                <Link to={`/projectBoard/${project.projectIdentifier}`}>
                   <li className="list-group-item board">
                     <i className="fa fa-flag-checkered pr-1">Project Board </i>
                   </li>
-                </a>
+                </Link>
                 {
                   // remember, to pass an parameter with url use the below syntax(back ticks and $ sign)
                 }
--- spring-react-project-fronted/src/reducers/index.js
+++ spring-react-project-fronted/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import ErrorReducer from "./ErrorReducer";
 import ProjectReducer from "./ProjectReducer";
+import ProjectTasksReducer from "./ProjectTasksReducer";
 
 export default combineReducers({
   errors: ErrorReducer,
   project: ProjectReducer,
+  backlog: ProjectTasksReducer,
 });
